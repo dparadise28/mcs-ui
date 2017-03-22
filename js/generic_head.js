2af24aa4359fad96c09d53ea650dd7065b47f485
David Paradis <dparadis28@gmail.com>
2017-03-22 16:08:50 -0400

    updates to code organization (starting a bit of cleanup for sanity)

--- js/generic_head.js
+++ js/generic_head.js
@@ -1,22 +1,27 @@
 document.head.innerHTML = `
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="../side-shopping-cart/js/modernizr.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-			integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-			crossorigin="anonymous">
-	</script>
+	<link rel="stylesheet" href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' type='text/css'>
 
-	<script src="../side-shopping-cart/js/modernizr.js"></script>
-
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
-
-	<title>MyCornerStore</title>
 	<link rel="stylesheet" href="../side-shopping-cart/css/reset.css">
 	<link rel="stylesheet" href="../side-shopping-cart/css/style.css">
-	<link rel="stylesheet" href="../css/login-modal.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-		  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-		  crossorigin="anonymous">
+
+	<title>MyCornerStore</title>
+	<script src="../side-shopping-cart/js/modernizr.js"></script>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				rel="stylesheet"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<link href="../css/intro_pg.css" rel="stylesheet">
+	<link href="../css/login-modal.css" rel="stylesheet">
+	<link href="../css/login_modal.css" rel="stylesheet">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+					integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+					crossorigin="anonymous"></script>
 `
--- js/generic_navbar.js
+++ js/generic_navbar.js
@@ -1,33 +1,27 @@
-document.body.innerHTML = `
+document.body.innerHTML += `
 	<header>
-
 		<a href="#"><div id="logo"><img src="../img/myCornerlogo.jpg" alt="Homepage" height="50" width="50"></div></a>
-
 		<div id="cd-hamburger-menu"><a class="cd-img-replace" href="#0">Menu</a></div>
 		<div id="cd-cart-trigger"><a class="cd-img-replace" href="#0">Cart</a></div>
 	</header>
+
 	<!-- Navigation -->
 	<nav id="main-nav">
-		<!--<div class="container-fluid">-->
-		<!--<div class="navbar-header">-->
-
 		<ul>
 			<li class="hidden"><a href="#page-top"></a></li>
 			<li><a class="page-scroll" href="#portfolio">Trending</a></li>
 			<li><a class="page-scroll" href="#team">Are You a Local Business</a></li>
 			<li><a class="page-scroll" href="#about">About</a></li>
 			<li><a class="page-scroll" href="#contact">Contact</a></li>
-			<li><a data-toggle="modal" href="#myModal"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			<li>
+				<a onclick="document.getElementById('id01').style.display='block'" style="width:auto;">
+					<span class="glyphicon glyphicon-log-in"></span>&nbsp &nbsp Login
+				</a>
+			</li>
 		</ul>
 		<button type="button" class="navbar-toggle login" data-toggle="collapse" data-target="#myNavbar">
 			<span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
 		</button>
-
-
-		<!--</div>-->
-
-
-		<!--</div>-->
 	</nav>
 
 	<div id="cd-shadow-layer"></div>
@@ -61,48 +55,44 @@ document.body.innerHTML = `
 
 		<p class="cd-go-to-cart"><a href="#0">Go to cart page</a></p>
 	</div> <!-- cd-cart -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="../side-shopping-cart/js/main.js"></script>
 
 
-	<!-----------Login Modal--------------------->
-	<div class="modal fade bs-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="lbOnoff">
-		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content">
-				<div class="modal-header" style="padding:35px 50px;">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+	<div id="id01" class="login_modal">
+	  <form class="login_modal_content login_modal_animate" action="cart.html">
+	    <div class="login_modal_imgcontainer">
+	      <span onclick="document.getElementById('id01').style.display='none'" class="login_modal_close" title="Close Modal">&times;</span>
+	      <h3 style="color: darkgreen;"><span class="glyphicon glyphicon-lock"></span> Login<h3>
+	    </div>
+
+	    <div class="login_modal_container">
+				<label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+				<input type="text" class="form-control" id="usrname" placeholder="Enter Username" name="uname" required>
+
+				<label form="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+				<input type="password" class="form-control" id="psw" placeholder="Enter password" name="psw" required>
+
+
+				<div class="checkbox">
+					<label><input type="checkbox" value="" checked>Remember me</label>
 				</div>
-				<div class="modal-body" style="padding:40px 50px;">
-					<form role="form">
-						<div class="form-group">
-							<label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
-							<input type="text" class="form-control" id="usrname" placeholder="Enter email">
-						</div>
-						<div class="form-group">
-							<label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
-							<input type="text" class="form-control" id="psw" placeholder="Enter password">
-						</div>
-						<div class="checkbox">
-							<label><input type="checkbox" value="" checked>Remember me</label>
-						</div>
-						<button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
-					</form>
-				</div>
-				<div class="modal-footer">
-					<button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal">
-					<span class="glyphicon glyphicon-remove"></span>Cancel</button>
-					<p>Not a member? <a href="#">Sign Up</a></p>
-					<p>Forgot <a href="#">Password?</a></p>
-				</div>
+				<button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
+
+	    </div>
+
+			<div class="modal-footer">
+				<button type="login_modal_button" class="btn btn-danger btn-default pull-left" data-dismiss="modal" onclick="document.getElementById('id01').style.display='none'" class="login_modal_cancelbtn">
+				<span class="glyphicon glyphicon-remove" onclick="document.getElementById('id01').style.display='none'" class="login_modal_cancelbtn"></span>Cancel</button>
+				<p>Not a member? <a href="#">Sign Up</a></p>
+				<p>Forgot <a href="#">Password?</a></p>
 			</div>
-		</div>
+	  </form>
 	</div>`
 
-$(window).resize(function () { 
-    $('body').css('padding-top', parseInt($('#main-navbar').css("height"))+85);
+$(window).on('resize', function () {
+    $('body').css('padding-top', '13vh');
 });
 
-$(window).load(function () { 
-    $('body').css('padding-top', parseInt($('#main-navbar').css("height"))+85);        
+$(window).on('load', function () {
+    $('body').css('padding-top', '13vh');
 });
