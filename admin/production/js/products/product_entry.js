7b5ccd7d06de70c247ef7326cfc1213ec57ec2dc
dparadise28 <dparadise28@gmail.com>
2017-04-03 21:45:49 +0000

    styling variant tables

--- admin/production/js/products/product_entry.js
+++ admin/production/js/products/product_entry.js
@@ -1,31 +1,33 @@
 var vm = new Vue({
-    el: '#productEntry',
-    name: 'mfgActivity',
-    data: {
-    	variants: [
-      	{
-        	name: "color",
-          id: 1,
-          options: [
-          	{option_name: "black"},
-            {option_name: "green"},
-            {option_name: "blue"}
-          ]
-        }, {
-        	name: "size",
-          id: 2,
-          options: [
-          	{option_name: "11"},
-            {option_name: "13"},
-            {option_name: "15"}
-          ]
-        }
-      ]
-    },
-    methods: {
-  		removeOption: function(variantId, optionName, options, index) {
-      	// Remove job from GUI
-        jobs.splice(index, 1);
-      }
-    }
+	el: '#productEntry',
+	name: 'mfgActivity',
+	data: {
+		product: {
+			variants: [
+				{
+					name: "color",
+					id: 1,
+					options: [
+						{option_name: "black"},
+						{option_name: "green"},
+						{option_name: "blue "}
+					]
+				}, {
+					name: "size",
+					id: 2,
+					options: [
+						{option_name: "11   "},
+						{option_name: "13   "},
+						{option_name: "15   "}
+					]
+				}
+			]
+		}
+	},
+	methods: {
+		removeOption: function(v_index, o_index) {
+			// Remove job from GUI
+			this.variants[v_index].options.splice(o_index, 1);
+		}
+	}
 })
